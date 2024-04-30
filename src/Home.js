d4a2847fb038197c6a9e8c71bafe9c576b9bf386
AnnixSilva <166154360+AnnixSilva@users.noreply.github.com>
2024-04-29 21:28:59 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,12 @@ function Home () {
     return(
         <div className='titulo'>
             <h1>Bem-vindo á página inicial da nossa AV2 </h1>
+            <br />
             <h3>Conheça os melhores Cosplays do SESI/SENAI.</h3>
+        </div>
 
             <div className='fotos'>
-                <img src={require('.foto1.png')} alt="Descrição da imagem" />
+                <img className ="FOTO1" src={require("../assets/FOTO1.jpg")} width={386}/>
             </div>
         </div>
     )
